d55b37d69d16bba41e2f409fcfe6856dc03e5736
Simon Gottipalli <simon.gottipalli@gmail.com>
2022-05-14 23:08:59 +0200

    handle cors

--- api/generated/restapi/configure_uuid_gen.go
+++ api/generated/restapi/configure_uuid_gen.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
+	"github.com/rs/cors"
 
 	"github.com/simongottipalli/uuid-gen/api/generated/restapi/operations"
 	"github.com/simongottipalli/uuid-gen/handlers"
@@ -66,5 +67,7 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	return handler
+	handleCORS := cors.Default().Handler
+
+	return handleCORS(handler)
 }

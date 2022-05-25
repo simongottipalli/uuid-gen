2a116279f0808b43e0ba9df718b34c510d3eeedd
simongottipalli <simon.gottipalli@gmail.com>
2022-05-25 18:57:50 +0200

    Refactor to lift state up

--- frontend/src/content/Uuid.js
+++ frontend/src/content/Uuid.js
@@ -2,41 +2,8 @@ import React from 'react'
 import {LinearProgress, Typography, Box, Button} from "@mui/material";
 import {CopyToClipboard} from "react-copy-to-clipboard/src";
 
-class Uuid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.basePath = "https://uuid-gen-g6d5qy6jra-ew.a.run.app"
-    this.genUUIDPath = "/uuid"
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: {}
-    };
-  }
-  componentDidMount() {
-    fetch(this.basePath + this.genUUIDPath)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
-  render() {
-    const { error, isLoaded, items } = this.state;
+export default function Uuid(props)  {
+    const {isLoaded, uuid, error} = props.item;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -47,26 +14,23 @@ class Uuid extends React.Component {
         )
     } else {
         return (
-          <Box component="span" sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 100,
-          }}>
-              {/*TODO: Show a message "Copied"*/}
-              <CopyToClipboard text={items.uuid}>
-                  <Button sx={{textTransform:'none'}}>
-                      <Typography variant="h3" fontWeight="bold">
-                           {items.uuid}
-                      </Typography>
-                  </Button>
+            <Box component="span" sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: 100,
+            }}>
+                {/*TODO: Show a message "Copied"*/}
+                <CopyToClipboard text={uuid}>
+                    <Button sx={{textTransform: 'none'}}>
+                        <Typography variant="h3" fontWeight="bold">
+                            {uuid}
+                        </Typography>
+                    </Button>
 
-              </CopyToClipboard>
+                </CopyToClipboard>
 
-          </Box>
-      );
+            </Box>
+        );
     }
-  }
 }
-
-export default Uuid;
--- frontend/src/content/UuidOptions.js
+++ frontend/src/content/UuidOptions.js
@@ -2,23 +2,36 @@ import {Checkbox, FormControlLabel, FormGroup} from "@mui/material";
 import * as React from "react";
 import Box from "@mui/material/Box";
 
-export default function UuidChoices() {
-    return (
-        <Box component="span" sx={{
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            height: 100,
-            alignItems: 'center',
-            p: 3,
-            m: 2,
-        }}>
-            <FormGroup row sx={{
-                justifyContent: 'space-evenly'
+class UuidChoices extends React.Component   {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+
+    handleChange()  {
+        this.props.onChange();
+    }
+
+    render()    {
+        return (
+            <Box component="span" sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                height: 100,
+                alignItems: 'center',
+                p: 3,
+                m: 2,
             }}>
-                <FormControlLabel control={<Checkbox />} label="Uppercase" />
-                <FormControlLabel control={<Checkbox />} label="Remove hyphens" />
-                <FormControlLabel control={<Checkbox />} label="Numbers only" />
-            </FormGroup>
-        </Box>
-    );
+                <FormGroup row sx={{
+                    justifyContent: 'space-evenly'
+                }}>
+                    <FormControlLabel control={<Checkbox />} label="Uppercase" onChange={this.handleChange} />
+                    <FormControlLabel control={<Checkbox />} label="Remove hyphens" />
+                    <FormControlLabel control={<Checkbox />} label="Numbers only" />
+                </FormGroup>
+            </Box>
+        );
+    }
 }
+export default UuidChoices
